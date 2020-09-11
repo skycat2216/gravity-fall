40a559216dd4273977ff98d0ebba902d6eeeb9e6
skycat2216 <33820902+skycat2216@users.noreply.github.com>
2020-09-11 12:05:24 +0800

    Initial commit

---
+++ Assets/player/playerctrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerctrl : MonoBehaviour
+{
+	    Transform trans;                                                         //-----b.
+    Rigidbody rigid;                                                      //-----c.
+    // Use this for initialization
+    void Start () {                                                               //-----d.
+        trans = GetComponent<Transform>();                   //-----e.
+        rigid = GetComponent<Rigidbody>();                //-----
+	
+}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {                  //-----h.
+            trans.Translate(Vector3.left * Time.deltaTime);   //-----i.
+        }else if (Input.GetKey(KeyCode.D)) {       //-----j.
+            trans.Translate(Vector3.right * Time.deltaTime); //-----k.
+        }else if (Input.GetKey(KeyCode.W)) {
+			trans.Translate(Vector3.forward * Time.deltaTime);
+		}else if (Input.GetKey(KeyCode.S)) {
+			trans.Translate(Vector3.down * Time.deltaTime);
+		}
+
+        if (Input.GetKeyDown(KeyCode.Backspace)) {          
+            rigid.AddForce(Vector3.up * 200);
+		}
+    }
+}
