ec6587a217c86398f8927c76e7ff44f12eb681b9
skycat2216 <33820902+skycat2216@users.noreply.github.com>
2020-09-26 12:01:03 +0800

    l3
    滑鼠鍵盤操作完成

---
+++ Assets/player/mousectrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousectrl : MonoBehaviour
+{
+	public Transform playerBody;
+	
+	public float mouseSensitve = 100f;
+	
+	float Xrotation;
+	float Yrotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = 	CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")*mouseSensitve*Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y")*mouseSensitve*Time.deltaTime;
+		
+		Xrotation = mouseX;
+		Xrotation = Mathf.Clamp(Xrotation,-90f,90f);
+		Yrotation = mouseY;
+		Yrotation = Mathf.Clamp(Yrotation,-180f,180f);
+		
+		transform.localRotation = Quaternion.Euler(Xrotation,0f,0f);
+		playerBody.Rotate(Vector3.up*mouseX);
+		transform.localRotation = Quaternion.Euler(0f,Yrotation,0f);
+		playerBody.Rotate(Vector3.right*mouseY);
+    }
+}
--- Assets/player/playerctrl.cs
+++ Assets/player/playerctrl.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class playerctrl : MonoBehaviour
 {    
-    public GameObject playercamera;
 	
-	
-	
-	void start()
-	{
+	public float playerspeed = 10f;
 
-	
-	
-	}
 	
 	void update()
 	{
+		// Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float x = Input.GetAxis("Vertical") * playerspeed;
+        float z = Input.GetAxis("Horizontal") * playerspeed;
 
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        x *= Time.deltaTime;
+        z *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, z);
+		transform.Translate(x, 0, 0);
+
+        // Rotate around our y-axis
+        
 	}
 }
