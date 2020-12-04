f2e4924c9a9090c91d0896d10a0e6990554acbf6
skycat2216 <33820902+skycat2216@users.noreply.github.com>
2020-12-04 12:00:26 +0800

    槍械腳本
    這次比一開始的移動腳本好很多
    至少沒卡住了

--- Assets/player/gunbehavior.cs
+++ Assets/player/gunbehavior.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class gunbehavior : MonoBehaviour
 {
     public Transform GunModel;
-    public Vector3 aimDonwnSight,hipFire; 	
+    
+	public Camera aimCam;
+    public Camera mainCam;
+ 	
 	public float gunrecoil = 50f;
+	public float firerate = 1f;
+	public float weaponRange = 50f;  
+	public Transform muzzle;
+	public GameObject copyGameObject;
+    public GameObject superGameObject;
+
+    private GameObject childGameObject;
 	
+	private LineRenderer laserLine;    
+	private float nextFire;  
 	
 	// Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
@@ -20,19 +32,26 @@ public class gunbehavior : MonoBehaviour
     {	
 		if(Input.GetKeyDown(KeyCode.Mouse0))
 		{
+			childGameObject = Instantiate(copyGameObject);
+			 
+            childGameObject.transform.parent = superGameObject.transform;
 			
+            childGameObject.transform.localPosition = muzzle.transform.position;
 		}
 		
         if(Input.GetKeyDown(KeyCode.Mouse1))
 		{
-			transform.localPosition = Vector3.Slerp
-				(transform.localPosition,aimDonwnSight,10*Time.deltaTime);
+			aimCam.enabled = true;
+			
+            mainCam.enabled = false;
 				
 			
 		}
 		if(Input.GetKeyUp(KeyCode.Mouse1))
 		{
-			transform.localPosition = hipFire;
+			aimCam.enabled = false;
+			
+            mainCam.enabled = true;
 		}
 		
 		
